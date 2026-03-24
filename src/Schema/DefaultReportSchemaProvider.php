199553eda343336c39ade951a64c5c250bd796b3
Daniel Dahme <danieldahme@googlemail.com>
2026-03-24 23:37:22 +0000

    license header

--- src/Schema/DefaultReportSchemaProvider.php
+++ src/Schema/DefaultReportSchemaProvider.php
@@ -1,5 +1,21 @@
 <?php declare(strict_types=1);
 
+/***********************************************************************
+ * This file is part of DataHawk for BASE3 Framework.
+ *
+ * DataHawk extends the BASE3 framework with a schema-driven query
+ * engine for reporting and data access. Queries are defined as
+ * structured JSON arrays, compiled into SQL, and executed through
+ * the BASE3 IDatabase abstraction.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de/v/datahawk
+ * https://github.com/ddbase3/DataHawk
+ **********************************************************************/
+
 namespace DataHawk\Schema;
 
 use Base3\Configuration\Api\IConfiguration;
